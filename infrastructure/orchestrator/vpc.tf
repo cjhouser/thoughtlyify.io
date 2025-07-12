e656871cdeffa07a6e01e6d6b1af8afcf435830f
Charles Houser <cjohouser@gmail.com>
2025-07-12 06:56:24 +0000

    dual-stack VPC for EKS
    EKS requires IPv4 addresses for the control plane. Hostnames must be
    resolvable for nodes to register with the control plane.

---
+++ infrastructure/orchestrator/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "platform" {
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  cidr_block                       = "10.0.0.0/20"
+  tags = {
+    Name = "platform"
+  }
+}
