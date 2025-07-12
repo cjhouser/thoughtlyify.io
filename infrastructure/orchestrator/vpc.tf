2c5a2c7a7c64092bad69c7950ba83412414f8f04
Charles Houser <cjohouser@gmail.com>
2025-07-12 20:07:05 +0000

    public subnets for N/S from private subnets
    The public subnets will contain load balancers that are controlled
    by Ingress objects that exist in the Kubernetes cluster.

--- infrastructure/orchestrator/vpc.tf
+++ infrastructure/orchestrator/vpc.tf
@@ -46,3 +46,45 @@ resource "aws_route_table" "private" {
     egress_only_gateway_id = aws_egress_only_internet_gateway.eigw.id
   }
 }
+
+resource "aws_subnet" "north_south_a" {
+  assign_ipv6_address_on_creation                = true
+  availability_zone                              = "us-west-2a"
+  cidr_block                                     = cidrsubnet(aws_vpc.platform.cidr_block, 3, 2)
+  enable_dns64                                   = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
+  ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 2)
+  map_public_ip_on_launch                        = true
+  vpc_id                                         = aws_vpc.platform.id
+  tags = {
+    Name = "north_south_a"
+  }
+}
+
+resource "aws_subnet" "north_south_b" {
+  assign_ipv6_address_on_creation                = true
+  availability_zone                              = "us-west-2b"
+  cidr_block                                     = cidrsubnet(aws_vpc.platform.cidr_block, 3, 3)
+  enable_dns64                                   = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
+  ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 3)
+  map_public_ip_on_launch                        = true
+  vpc_id                                         = aws_vpc.platform.id
+  tags = {
+    Name = "north_south_b"
+  }
+}
+
+resource "aws_subnet" "north_south_c" {
+  assign_ipv6_address_on_creation                = true
+  availability_zone                              = "us-west-2c"
+  cidr_block                                     = cidrsubnet(aws_vpc.platform.cidr_block, 3, 4)
+  enable_dns64                                   = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
+  ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 4)
+  map_public_ip_on_launch                        = true
+  vpc_id                                         = aws_vpc.platform.id
+  tags = {
+    Name = "north_south_c"
+  }
+}
