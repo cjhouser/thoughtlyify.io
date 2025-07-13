e1e6c3e4d627beb2f37e135a67849868bd0fe768
Charles Houser <cjohouser@gmail.com>
2025-07-13 16:35:02 +0000

    EKS node role
    These are required permissions for the EKS node role for an IPv6
    cluster. Not the ideal set up; AWS recommends using a separate role
    for each addon.

--- infrastructure/orchestrator/data.tf
+++ infrastructure/orchestrator/data.tf
@@ -16,3 +16,43 @@ data "aws_iam_policy_document" "authn_eks_cluster" {
 data "aws_iam_user" "chouser" {
   user_name = "charles.houser"
 }
+
+data "aws_iam_policy_document" "authn_eks_node" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "ec2.amazonaws.com"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "authz_ipv6_cni" {
+  statement {
+    actions = [
+      "ec2:AssignIpv6Addresses",
+      "ec2:DescribeInstances",
+      "ec2:DescribeTags",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeInstanceTypes"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+  statement {
+    actions = [
+      "ec2:CreateTags"
+    ]
+    effect = "Allow"
+    resources = [
+      "arn:aws:ec2:*:*:network-interface/*"
+    ]
+  }
+}
