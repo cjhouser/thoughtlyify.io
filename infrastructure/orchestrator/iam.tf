09044ef275ab9161bb2900294007c9e1f03186d1
Charles Houser <cjohouser@gmail.com>
2025-07-13 16:35:02 +0000

    EKS node role
    These are required permissions for the EKS node role for an IPv6
    cluster. Not the ideal set up; AWS recommends using a separate role
    for each addon.

--- infrastructure/orchestrator/iam.tf
+++ infrastructure/orchestrator/iam.tf
@@ -7,3 +7,35 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
+
+resource "aws_iam_role" "eks_node" {
+  assume_role_policy = data.aws_iam_policy_document.authn_eks_node.json
+  name               = "eks_node"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_pull_only" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ipv6_cni" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = aws_iam_policy.ipv6_cni.arn
+}
+
+resource "aws_iam_policy" "ipv6_cni" {
+  description = "ipv6 CNI EKS"
+  name        = "ipv6_cni"
+  path        = "/"
+  policy      = data.aws_iam_policy_document.authz_ipv6_cni.json
+}
