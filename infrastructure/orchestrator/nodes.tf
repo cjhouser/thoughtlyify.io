1a01114c1dc7e53b2592b08bba70c008f43475ce
Charles Houser <cjohouser@gmail.com>
2025-09-16 03:24:32 +0000

    tf fmt

--- infrastructure/orchestrator/nodes.tf
+++ infrastructure/orchestrator/nodes.tf
@@ -7,7 +7,7 @@ resource "aws_subnet" "nodes_a" {
   ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 5)
   vpc_id                                         = aws_vpc.platform.id
   tags = {
-    Name                              = "nodes_a"
+    Name = "nodes_a"
     "kubernetes.io/role/internal-elb" = 1
   }
 }
@@ -21,7 +21,7 @@ resource "aws_subnet" "nodes_b" {
   ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 6)
   vpc_id                                         = aws_vpc.platform.id
   tags = {
-    Name                              = "nodes_b"
+    Name = "nodes_b"
     "kubernetes.io/role/internal-elb" = 1
   }
 }
@@ -35,7 +35,7 @@ resource "aws_subnet" "nodes_c" {
   ipv6_cidr_block                                = cidrsubnet(aws_vpc.platform.ipv6_cidr_block, 8, 7)
   vpc_id                                         = aws_vpc.platform.id
   tags = {
-    Name                              = "nodes_c"
+    Name = "nodes_c"
     "kubernetes.io/role/internal-elb" = 1
   }
 }
