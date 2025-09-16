2ee805cf714da443879a8f6e24e567982da78dde
Charles Houser <cjohouser@gmail.com>
2025-09-16 02:35:00 +0000

    oidc arn has multiple slashes

--- infrastructure/orchestrator/main.tf
+++ infrastructure/orchestrator/main.tf
@@ -17,7 +17,8 @@ terraform {
 }
 
 locals {
-  short_oidc_arn = split("/", aws_iam_openid_connect_provider.platform.arn)[1]
+  oidc_array     = split("/", aws_iam_openid_connect_provider.platform.arn)
+  short_oidc_arn = join("/", slice(local.oidc_array, 1, length(local.oidc_array)))
   k8s_common_annotations = {
     "thoughtlyify.io/directory"  = "infrastructure/orchestrator/"
     "thoughtlyify.io/repository" = "github.com/cjhouser/thoughtlyify.io"
