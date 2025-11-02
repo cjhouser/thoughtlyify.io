027fce69791cbee12edfe25aef127ecf4df5a487
Charles Houser <cjohouser@gmail.com>
2025-11-02 04:23:48 +0000

    EBS volumes are zonal resources
    A node must be available in the same zone as the EBS volume before
    a Pod that needs the PVC can be scheduled.

--- infrastructure/orchestrator/eks.tf
+++ infrastructure/orchestrator/eks.tf
@@ -220,9 +220,9 @@ resource "aws_eks_node_group" "persistence" {
   ]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 3
+    max_size     = 4
+    min_size     = 3
   }
 
   taint {
