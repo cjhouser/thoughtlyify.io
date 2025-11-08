3db0c629cf787257965c82db8392bcc389edd334
Charles Houser <cjohouser@gmail.com>
2025-11-08 03:17:52 +0000

    reduce cost by reducing persistence scale

--- infrastructure/orchestrator/eks.tf
+++ infrastructure/orchestrator/eks.tf
@@ -249,9 +249,9 @@ resource "aws_eks_node_group" "persistence" {
   ]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 4
-    min_size     = 3
+    desired_size = 1
+    max_size     = 1
+    min_size     = 0
   }
 
   taint {
