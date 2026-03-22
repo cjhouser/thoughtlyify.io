4c04f8671f11d1043f69af90eaa86466633011ed
Charles Houser <cjohouser@gmail.com>
2026-03-22 01:15:58 +0000

    refactor: rename vnet

--- infrastructure/azure/base/main.tf
+++ infrastructure/azure/base/main.tf
@@ -49,8 +49,8 @@ resource "azurerm_resource_group" "platform" {
   location = data.azurerm_location.centralus.location
 }
 
-resource "azurerm_virtual_network" "platform" {
-  name                = "platform"
+resource "azurerm_virtual_network" "platform_a" {
+  name                = "platform_a"
   location            = azurerm_resource_group.platform.location
   resource_group_name = azurerm_resource_group.platform.name
   address_space = [
@@ -61,7 +61,7 @@ resource "azurerm_virtual_network" "platform" {
 resource "azurerm_subnet" "nodes" {
   name                 = "nodes"
   resource_group_name  = azurerm_resource_group.platform.name
-  virtual_network_name = azurerm_virtual_network.platform.name
+  virtual_network_name = azurerm_virtual_network.platform_a.name
   address_prefixes = [
     cidrsubnet(local.platform_vnet_a, 4, 0)
   ]
