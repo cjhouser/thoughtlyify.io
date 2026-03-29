abc18e36db87378d4e39d889cd3aa51bbf1d71e7
Charles Houser <cjohouser@gmail.com>
2026-03-29 23:53:06 +0000

    refactor: hub vnet gets its own file

---
+++ infrastructure/azure/base/hub.tf
@@ -0,0 +1,72 @@
+##############
+### egress ###
+##############
+resource "azurerm_nat_gateway" "egress_a" {
+  name                = "egress_a"
+  location            = azurerm_resource_group.platform.location
+  resource_group_name = azurerm_resource_group.platform.name
+  zones               = ["1"]
+}
+
+resource "azurerm_public_ip" "egress_a" {
+  name                    = "egress_a"
+  location                = azurerm_resource_group.platform.location
+  resource_group_name     = azurerm_resource_group.platform.name
+  allocation_method       = "Static"
+  sku                     = "Standard"
+  idle_timeout_in_minutes = 4
+  zones                   = ["1"]
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "egress_a_egress_a" {
+  nat_gateway_id       = azurerm_nat_gateway.egress_a.id
+  public_ip_address_id = azurerm_public_ip.egress_a.id
+}
+
+resource "azurerm_network_interface" "nva_a" {
+  name                  = "nva_a"
+  location              = azurerm_resource_group.platform.location
+  resource_group_name   = azurerm_resource_group.platform.name
+  ip_forwarding_enabled = true
+
+  ip_configuration {
+    name                          = "nva_a"
+    subnet_id                     = azurerm_subnet.egress_hub_a.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = local.nva_egress_hub_network_a
+    primary                       = true
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nva_a.id
+  network_security_group_id = azurerm_network_security_group.nva_a.id
+}
+
+# simulate an NVA so i don't have to pay for azure firewall
+resource "azurerm_linux_virtual_machine" "nva_a" {
+  name                            = "nva-a"
+  resource_group_name             = azurerm_resource_group.platform.name
+  location                        = azurerm_resource_group.platform.location
+  size                            = "Standard_D2pds_v6"
+  encryption_at_host_enabled      = true
+  admin_username                  = "nva_admin"
+  admin_password                  = var.admin_password_nva_a
+  disable_password_authentication = false
+  custom_data                     = base64encode(file("cloud-config.yaml"))
+  network_interface_ids = [
+    azurerm_network_interface.nva_a.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-arm64"
+    version   = "latest"
+  }
+}
--- infrastructure/azure/base/main.tf
+++ infrastructure/azure/base/main.tf
@@ -23,29 +23,6 @@ resource "azurerm_resource_group" "platform" {
   name     = "platform"
   location = data.azurerm_location.centralus.location
 }
-# Provider does not support standardv2 nat gateway
-resource "azurerm_nat_gateway" "egress_a" {
-  name                = "egress_a"
-  location            = azurerm_resource_group.platform.location
-  resource_group_name = azurerm_resource_group.platform.name
-  zones               = ["1"]
-}
-
-resource "azurerm_public_ip" "egress_a" {
-  name                    = "egress_a"
-  location                = azurerm_resource_group.platform.location
-  resource_group_name     = azurerm_resource_group.platform.name
-  allocation_method       = "Static"
-  sku                     = "Standard"
-  idle_timeout_in_minutes = 4
-  zones                   = ["1"]
-}
-
-resource "azurerm_nat_gateway_public_ip_association" "egress_a_egress_a" {
-  nat_gateway_id       = azurerm_nat_gateway.egress_a.id
-  public_ip_address_id = azurerm_public_ip.egress_a.id
-}
-
 resource "azurerm_subnet_nat_gateway_association" "pubic_hub_a_egress_a" {
   subnet_id      = azurerm_subnet.public_hub_a.id
   nat_gateway_id = azurerm_nat_gateway.egress_a.id
