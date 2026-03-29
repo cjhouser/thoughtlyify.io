5a659e2f690923d9fd0dfa8a1a15a6f812151612
Charles Houser <cjohouser@gmail.com>
2026-03-29 23:43:41 +0000

    refactor: move vnet,subnet,peering to network.tf

--- infrastructure/azure/base/main.tf
+++ infrastructure/azure/base/main.tf
@@ -23,60 +23,6 @@ resource "azurerm_resource_group" "platform" {
   name     = "platform"
   location = data.azurerm_location.centralus.location
 }
-
-resource "azurerm_virtual_network" "hub_a" {
-  name                = "hub_a"
-  location            = azurerm_resource_group.platform.location
-  resource_group_name = azurerm_resource_group.platform.name
-  address_space = [
-    local.hub_vnet_a
-  ]
-}
-
-resource "azurerm_subnet" "hub_a" {
-  name                 = "hub_a"
-  resource_group_name  = azurerm_resource_group.platform.name
-  virtual_network_name = azurerm_virtual_network.hub_a.name
-  address_prefixes = [
-    cidrsubnet(local.hub_vnet_a, 4, 0)
-  ]
-}
-
-resource "azurerm_virtual_network" "platform_a" {
-  name                = "platform_a"
-  location            = azurerm_resource_group.platform.location
-  resource_group_name = azurerm_resource_group.platform.name
-  address_space = [
-    local.platform_vnet_a
-  ]
-}
-
-resource "azurerm_subnet" "nodes_platform_a" {
-  name                            = "nodes_platform_a"
-  default_outbound_access_enabled = false
-  resource_group_name             = azurerm_resource_group.platform.name
-  virtual_network_name            = azurerm_virtual_network.platform_a.name
-  address_prefixes = [
-    cidrsubnet(local.platform_vnet_a, 4, 0)
-  ]
-}
-
-resource "azurerm_virtual_network_peering" "hub_a_to_platform_a" {
-  name                      = "hub_a_to_platform_a"
-  resource_group_name       = azurerm_resource_group.platform.name
-  virtual_network_name      = azurerm_virtual_network.hub_a.name
-  remote_virtual_network_id = azurerm_virtual_network.platform_a.id
-  allow_forwarded_traffic   = true
-}
-
-resource "azurerm_virtual_network_peering" "platform_a_to_hub_a" {
-  name                      = "platform_a_to_hub_a"
-  resource_group_name       = azurerm_resource_group.platform.name
-  virtual_network_name      = azurerm_virtual_network.platform_a.name
-  remote_virtual_network_id = azurerm_virtual_network.hub_a.id
-  allow_forwarded_traffic   = true
-}
-
 # Provider does not support standardv2 nat gateway
 resource "azurerm_nat_gateway" "egress_a" {
   name                = "egress_a"
--- infrastructure/azure/base/network.tf
+++ infrastructure/azure/base/network.tf
@@ -21,3 +21,70 @@ locals {
 
   nodes_platform_network_a = cidrsubnet(local.platform_network_a, 3, 0)
 }
+
+
+#############
+### hub_a ###
+#############
+resource "azurerm_virtual_network" "hub_a" {
+  name                = "hub_a"
+  location            = azurerm_resource_group.platform.location
+  resource_group_name = azurerm_resource_group.platform.name
+  address_space = [
+    local.hub_network_a
+  ]
+}
+
+resource "azurerm_subnet" "egress_hub_a" {
+  name                 = "egress_hub_a"
+  resource_group_name  = azurerm_resource_group.platform.name
+  virtual_network_name = azurerm_virtual_network.hub_a.name
+  address_prefixes = [
+    local.egress_hub_network_a
+  ]
+}
+
+
+##################
+### platform_a ###
+##################
+resource "azurerm_virtual_network" "platform_a" {
+  name                = "platform_a"
+  location            = azurerm_resource_group.platform.location
+  resource_group_name = azurerm_resource_group.platform.name
+  address_space = [
+    local.platform_network_a
+  ]
+}
+
+resource "azurerm_subnet" "nodes_platform_a" {
+  name                            = "nodes_platform_a"
+  default_outbound_access_enabled = false
+  resource_group_name             = azurerm_resource_group.platform.name
+  virtual_network_name            = azurerm_virtual_network.platform_a.name
+  address_prefixes = [
+    local.nodes_platform_network_a
+  ]
+}
+
+
+###############
+### peering ###
+###############
+resource "azurerm_virtual_network_peering" "hub_a_to_platform_a" {
+  name                      = "hub_a_to_platform_a"
+  resource_group_name       = azurerm_resource_group.platform.name
+  virtual_network_name      = azurerm_virtual_network.hub_a.name
+  remote_virtual_network_id = azurerm_virtual_network.platform_a.id
+  allow_forwarded_traffic   = true
+}
+
+resource "azurerm_virtual_network_peering" "platform_a_to_hub_a" {
+  name                      = "platform_a_to_hub_a"
+  resource_group_name       = azurerm_resource_group.platform.name
+  virtual_network_name      = azurerm_virtual_network.platform_a.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_a.id
+  allow_forwarded_traffic   = true
+}
+
+
