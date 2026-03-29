e7fcfb4cf6cf91352f55f9e804547777716dc71b
Charles Houser <cjohouser@gmail.com>
2026-03-29 23:55:40 +0000

    feat: pw auth after hopping from bastion

--- infrastructure/azure/base/variables.tf
+++ infrastructure/azure/base/variables.tf
@@ -2,3 +2,9 @@ variable "engineering_subscription_id" {
   type      = string
   sensitive = true
 }
+
+variable "admin_password_nva_a" {
+  description = "admin password for the simulated NVA"
+  type        = string
+  sensitive   = true
+}
